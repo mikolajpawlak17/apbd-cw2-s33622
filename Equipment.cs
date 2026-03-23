bb06ab6c44ae879b945238f3bd73e88afe3ffeb1
Mikołaj Pawlak <s33622@pjwstk.edu.pl>
2026-03-23 20:49:55 +0100

    Add project structure and abstract class Equipment

---
+++ Equipment.cs
@@ -0,0 +1,16 @@
+﻿namespace apbd_cw2_s33622;
+
+public abstract class Equipment
+
+{
+    public string Id { get; private set; }
+    public string Name { get; set; }
+    public bool Available { get; set; }
+
+    public Equipment(string name)
+    {
+        Id = Guid.NewGuid().ToString();
+        Name = name;
+        Available = true;
+    }
+}
